0374db5029e828bffbab9c8028fca8ed1455333d
Alberto Ojeda <ojedamtz.alberto@gmail.com>
2016-10-08 21:10:46 -0600

    Se agregan campos que faltaban de la API de pokemon

---
+++ database/migrations/2016_10_08_234319_create_test_tabl.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestTabl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test', function (Blueprint $table) {
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
